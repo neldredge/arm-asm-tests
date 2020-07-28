eff1ed63b2b415b0952565558c204a38934e2490
Nate Eldredge <nate@thatsmathematics.com>
2020-07-28 13:51:08 +0000

    Unnecessary to store/load zero register

--- hello/hello.s
+++ hello/hello.s
@@ -4,9 +4,9 @@ message:
 	.text
 	.globl main
 main:
-	// push link register, also zero register to make a pair
-	// stack must be aligned to 16 bytes so we must always push a pair
-	stp xzr, x30, [sp, #-16]!
+	// push link register
+	// stack must be aligned to 16 bytes
+	str x30, [sp, #-16]!
 
 	.extern puts
 	// add pc to offset of message and store in x0
@@ -15,6 +15,6 @@ main:
 
 	// zero w0 for return value
 	mov w0, wzr
-	ldp xzr, x30, [sp], #16
+	ldr x30, [sp], #16
 	ret
 	
