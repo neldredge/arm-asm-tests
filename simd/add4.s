fdb394e6cd3a39231e085835fa568fb4fa1642b5
Nate Eldredge <nate@thatsmathematics.com>
2020-08-09 06:17:37 +0000

    disable faddv for now

--- simd/add4.s
+++ simd/add4.s
@@ -9,10 +9,10 @@ add4i_asm:
 
 add4f_asm:
 	// The floating-point version is an extension not available on all CPUS
-	
+
+	//.arch armv8.5-a+sve
 	//ldr q0, [x0]
 	//ptrue p0.s
-	//faddv s0, p0, v0.4s
-	fmov s0, wzr
+	//faddv s0, p0, z0.4s
 	ret
 
--- simd/main.c
+++ simd/main.c
@@ -9,24 +9,26 @@ float add4f_c(const float *a) {
 }
 
 extern int add4i_asm(const int *a);
-extern float add4f_asm(const float *a);
+//extern float add4f_asm(const float *a);
 
 int main(void) {
   static int ai[] = {1, 20, 300, 4000};
-  static float af[] = {80.0, 7.0, 0.6, 0.05};
+  //static float af[] = {80.0, 7.0, 0.6, 0.05};
 
   int aic = add4i_c(ai);
   int ais = add4i_asm(ai);
 
-  float afc = add4f_c(af);
-  float afs = add4f_asm(af);
+  //float afc = add4f_c(af);
+  //float afs = add4f_asm(af);
   
   printf("int %s : c = %d, asm = %d\n",
 	 (aic == ais) ? "OK" : "FAIL",
 	 aic, ais);
+#if 0
   printf("float %s : c = %f, asm = %f\n",
 	 (afc == afs) ? "OK" : "FAIL",
 	 afc, afs);
+#endif
   return 0;
 }
 
